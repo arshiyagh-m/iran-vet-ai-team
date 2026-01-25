9214ff675e57f66bed3909a54cc5a06632f927ef
Arshiya ghanbari <246164980+arshiyagh-m@users.noreply.github.com>
2026-01-26 03:19:39 +0330

    Create vite.config.js

---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      // هر درخواستی که با /api شروع شود را به بک‌اند بفرست
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
+
