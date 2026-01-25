8d44f49c2347d925ac59e2bfa5752bda1905fa2d
Arshiya ghanbari <246164980+arshiyagh-m@users.noreply.github.com>
2026-01-26 03:21:47 +0330

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // اتصال استایل‌های Tailwind و فونت‌ها
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
