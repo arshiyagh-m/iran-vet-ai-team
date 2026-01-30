b3f3dc5c3e9c914e39805ac75ad1d34ffe4a38f3
Arshiya ghanbari <246164980+arshiyagh-m@users.noreply.github.com>
2026-01-31 03:03:29 +0330

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  css: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
   build: {
     outDir: 'dist',
   }
