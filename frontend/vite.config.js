479e7373a6ef9efe48aa24989ac92cca9d62acfe
Arshiya ghanbari <246164980+arshiyagh-m@users.noreply.github.com>
2026-01-31 01:31:38 +0330

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  server: {
-    proxy: {
-      // هر درخواستی که با /api شروع شود را به بک‌اند بفرست
-      '/api': {
-        target: 'https://iran-vet-ai-team.onrender.com/',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  base: './', // تغییر مهم: اضافه کردن نقطه قبل از اسلش
+  build: {
+    outDir: 'dist',
+  }
 })
