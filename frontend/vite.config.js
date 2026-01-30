fd7dd7b88ddb3dedd510eaf0b9d31e54433acb59
Arshiya ghanbari <246164980+arshiyagh-m@users.noreply.github.com>
2026-01-31 00:56:31 +0330

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',   // <--- این خط حیاتی است!
+})
+,
   server: {
     proxy: {
       // هر درخواستی که با /api شروع شود را به بک‌اند بفرست
