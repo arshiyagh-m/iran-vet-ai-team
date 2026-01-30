4fd893a521319f3673d2a1f325f9f5fbcdbb9459
Arshiya ghanbari <246164980+arshiyagh-m@users.noreply.github.com>
2026-01-31 02:08:36 +0330

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
-alert("سایت بیدار شد!");
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // اتصال استایل‌های Tailwind و فونت‌ها
+import './index.css' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
