3203d35c41238e612a5645f2d81ddf842662550c
Arshiya ghanbari <246164980+arshiyagh-m@users.noreply.github.com>
2026-02-02 12:55:03 +0330

    Update Ticket.js

--- backend/models/Ticket.js
+++ backend/models/Ticket.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 
-const ticketSchema = new mongoose.Schema({
+const TicketSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     subject: { type: String, required: true },
     message: { type: String, required: true },
-    reply: { type: String }, // پاسخ ادمین
-    status: { type: String, enum: ['Open', 'Closed', 'Answered'], default: 'Open' },
-    createdAt: { type: Date, default: Date.now }
+    reply: { type: String },
+    status: { type: String, enum: ['Open', 'Answered', 'Closed'], default: 'Open' },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Ticket', ticketSchema);
-
+module.exports = mongoose.model('Ticket', TicketSchema);
