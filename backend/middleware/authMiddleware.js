c8574ac2b5adc40fe6c9ab8a521c5cb034aa731c
Arshiya ghanbari <246164980+arshiyagh-m@users.noreply.github.com>
2026-02-02 12:52:43 +0330

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,30 +2,30 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 const protect = async (req, res, next) => {
-    let token;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        try {
-            token = req.headers.authorization.split(' ')[1];
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = await User.findById(decoded.id).select('-password');
-            next();
-        } catch (error) {
-            res.status(401).json({ message: 'عدم دسترسی، توکن نامعتبر است' });
-        }
-    }
+  let token;
 
-    if (!token) {
-        res.status(401).json({ message: 'عدم دسترسی، توکن یافت نشد' });
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (error) {
+      res.status(401).json({ message: 'توکن نامعتبر است، لطفا دوباره وارد شوید.' });
     }
+  }
+
+  if (!token) {
+    res.status(401).json({ message: 'دسترسی غیرمجاز، توکن یافت نشد.' });
+  }
 };
 
 const admin = (req, res, next) => {
-    if (req.user && req.user.role === 'admin') {
-        next();
-    } else {
-        res.status(403).json({ message: 'دسترسی غیرمجاز! فقط ادمین‌ها مجاز هستند.' });
-    }
+  if (req.user && req.user.role === 'admin') {
+    next();
+  } else {
+    res.status(403).json({ message: 'دسترسی محدود به مدیران سیستم است.' });
+  }
 };
 
 module.exports = { protect, admin };
-
